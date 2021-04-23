f5ed01739c51c8093ba107fc9017a8032cbe799c
rodrigo <rdsouza.c@gmail.com>
2021-04-22 20:13:49 -0400

    Projeto souzafood-api

--- souzafood-api/src/main/java/com/souza/souzafood/api/controller/CozinhaController.java
+++ souzafood-api/src/main/java/com/souza/souzafood/api/controller/CozinhaController.java
@@ -6,7 +6,9 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -43,10 +45,15 @@ public class CozinhaController {
 	private CozinhaInputDisassembler cozinhaInputDisassembler;
 	
 	@GetMapping
-	public List<CozinhaModel> listar(Pageable pageable) {
+	public Page<CozinhaModel> listar(@PageableDefault(size = 10) Pageable pageable) {
 		       Page<Cozinha> cozinhasPage = cozinhaRepository.findAll(pageable);
-			return cozinhaModelAssembler.toCollectionModel(cozinhasPage.getContent());
-	}
+			List<CozinhaModel> cozinhasModel = cozinhaModelAssembler.toCollectionModel(cozinhasPage.getContent());
+	
+	     Page<CozinhaModel> cozinhasModelPage = new PageImpl<>(cozinhasModel, pageable,
+	    		 cozinhasPage.getTotalElements());
+	     
+	     return cozinhasModelPage;
+ 	}
 	
 	@GetMapping("/{cozinhaId}")
 	public CozinhaModel buscar(@PathVariable Long cozinhaId) {
