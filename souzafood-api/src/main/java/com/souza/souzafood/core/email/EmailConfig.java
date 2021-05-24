01c884a1c9233914801635f44a8524c95f02ad02
rodrigo <rdsouza.c@gmail.com>
2021-05-24 14:55:15 -0400

    Projeto souzafood-api

--- souzafood-api/src/main/java/com/souza/souzafood/core/email/EmailConfig.java
+++ souzafood-api/src/main/java/com/souza/souzafood/core/email/EmailConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.souza.souzafood.domain.service.EnvioEmailService;
 import com.souza.souzafood.infrastructure.service.email.FakeEnvioEmailService;
+import com.souza.souzafood.infrastructure.service.email.SandboxEnvioEmailService;
 import com.souza.souzafood.infrastructure.service.email.SmtpEnvioEmailService;
 
 @Configuration
@@ -22,6 +23,8 @@ public class EmailConfig {
                 return new FakeEnvioEmailService();
             case SMTP:
                 return new SmtpEnvioEmailService();
+            case SANDBOX:
+                return new SandboxEnvioEmailService();
             default:
                 return null;
         }
