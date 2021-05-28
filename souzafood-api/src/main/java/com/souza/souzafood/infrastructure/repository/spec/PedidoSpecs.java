b336a159348d4e56ae9fd131f7c8f5b043d202be
rodrigo <rdsouza.c@gmail.com>
2021-05-28 17:20:25 -0400

    Projeto souzafood-api

--- souzafood-api/src/main/java/com/souza/souzafood/infrastructure/repository/spec/PedidoSpecs.java
+++ souzafood-api/src/main/java/com/souza/souzafood/infrastructure/repository/spec/PedidoSpecs.java
@@ -12,7 +12,21 @@ import com.souza.souzafood.domain.model.Pedido;
 public class PedidoSpecs {
 
 	public static Specification<Pedido> usandoFiltro(PedidoFilter filtro) {
-//Para entender esse retorno com os parametros root,query e builder veja a aula: https://www.algaworks.com/aulas/1891/criando-uma-fabrica-de-specifications
+// Vamos fazer o retorno de outra forma sem implementar a interface Specification em uma classe para poder assim usar o seu metodo toPredicate
+// A idéia é não precisar criar uma classe que implemente a interface
+// Specification e sim ter apenas uma classe como esta retornando o Specification diretamente nos
+// seus metodos.
+// Uma inteface do tipo Specification retorna um metodo toPredicate e esse metodo recebe 
+// por parametro: Root<T> root, CriteriaQuery<?> query, CriteriaBuilder criteriaBuilder
+// Ficando assim:
+// Predicate toPredicate(Root<T> root, CriteriaQuery<?> query, CriteriaBuilder criteriaBuilder);
+// Então criamos um expressão lambda onde vai ser recebido como parametros o root, query e builder
+// assim como no metodo toPredicate. E no corpo da instancia a ser retornada, retornamos um predicate
+// apartir do builder
+// Dessa forma temos um metodo "toPredicate" retornado para o Specification
+// Podemos até apagar a classe RestauranteComFreteGratisSpec pois ela não é mas útil já que agora
+// Usamos essa classe RestauranteSpecs e nela ja retornamos o Specification diretamente nos metodos
+//aula: https://www.algaworks.com/aulas/1891/criando-uma-fabrica-de-specifications
 		return (root, query, builder) -> { //O parametro query é o CriteriaQuery
 			if (Pedido.class.equals(query.getResultType())) { //if usado para resolver 
 // a exception: org.springframework.dao.InvalidDataAccessApiUsageException: org.hibernate.QueryException: query specified join fetching, but the owner of the fetched association was not present in the select list [FromElement{explicit,not a collection join,fetch join,fetch non-lazy properties,classAlias=generatedAlias1,role=com.souza.souzafood.domain.model.Pedido.restaurante,tableName=restaurante,tableAlias=restaurant1_,origin=pedido pedido0_,columns={pedido0_.restaurante_id ,className=com.souza.souzafood.domain.model.Restaurante}}] [select count(generatedAlias0) from com.souza.souzafood.domain.model.Pedido as generatedAlias0 inner join fetch generatedAlias0.restaurante as generatedAlias1 inner join fetch generatedAlias1.cozinha as generatedAlias2 inner join fetch generatedAlias0.cliente as generatedAlias3 where generatedAlias0.restaurante=1L]; nested exception is java.lang.IllegalArgumentException: org.hibernate.QueryException: query specified join fetching, but the owner of the fetched association was not present in the select list [FromElement{explicit,not a collection join,fetch join,fetch non-lazy properties,classAlias=generatedAlias1,role=com.souza.souzafood.domain.model.Pedido.restaurante,tableName=restaurante,tableAlias=restaurant1_,origin=pedido pedido0_,columns={pedido0_.restaurante_id ,className=com.souza.souzafood.domain.model.Restaurante}}] [select count(generatedAlias0) from com.souza.souzafood.domain.model.Pedido as generatedAlias0 inner join fetch generatedAlias0.restaurante as generatedAlias1 inner join fetch generatedAlias1.cozinha as generatedAlias2 inner join fetch generatedAlias0.cliente as generatedAlias3 where generatedAlias0.restaurante=1L]
--- souzafood-api/src/main/java/com/souza/souzafood/infrastructure/repository/spec/RestauranteSpecs.java
+++ souzafood-api/src/main/java/com/souza/souzafood/infrastructure/repository/spec/RestauranteSpecs.java
@@ -13,20 +13,20 @@ public class RestauranteSpecs {
 		
   //return new RestauranteComFreteGratisSpec();	
 	
-	// Vamos fazer o retorno de outra forma sem usar a classe RestauranteComFreteGratisSpec 
-	// na instanciação no retorno. A idéia é não precisar criar uma classe que implemente a interface
-	// Specification e sim ter apenas uma classe como esta retornando o Specification diretamente nos
-	// seus metodos.
-	// Uma inteface do tipo Specification retorna um metodo toPredicate e esse metodo recebe 
-	// por parametro: Root<T> root, CriteriaQuery<?> query, CriteriaBuilder criteriaBuilder
-	// Ficando assim:
-	// Predicate toPredicate(Root<T> root, CriteriaQuery<?> query, CriteriaBuilder criteriaBuilder);
-	// Então criamos um expressão lambda onde vai ser recebido como parametros o root, query e builder
-	// assim como no metodo toPredicate. E no corpo da instancia a ser retornada, retornamos um predicate
-	// apartir do builder
-	// Dessa forma temos um metodo "toPredicate" retornado para o Specification
-	// Podemos até apagar a classe RestauranteComFreteGratisSpec pois ela não é mas útil já que agora
-	// Usamos essa classe RestauranteSpecs e nela ja retornamos o Specification diretamente nos metodos
+// Vamos fazer o retorno de outra forma sem usar a classe RestauranteComFreteGratisSpec 
+// na instanciação no retorno. A idéia é não precisar criar uma classe que implemente a interface
+// Specification e sim ter apenas uma classe como esta retornando o Specification diretamente nos
+// seus metodos.
+// Uma inteface do tipo Specification retorna um metodo toPredicate e esse metodo recebe 
+// por parametro: Root<T> root, CriteriaQuery<?> query, CriteriaBuilder criteriaBuilder
+// Ficando assim:
+// Predicate toPredicate(Root<T> root, CriteriaQuery<?> query, CriteriaBuilder criteriaBuilder);
+// Então criamos um expressão lambda onde vai ser recebido como parametros o root, query e builder
+// assim como no metodo toPredicate. E no corpo da instancia a ser retornada, retornamos um predicate
+// apartir do builder
+// Dessa forma temos um metodo "toPredicate" retornado para o Specification
+// Podemos até apagar a classe RestauranteComFreteGratisSpec pois ela não é mas útil já que agora
+// Usamos essa classe RestauranteSpecs e nela ja retornamos o Specification diretamente nos metodos
 		
 	  return (root, query, builder) -> //Operador
 		  builder.equal(root.get("taxaFrete"), BigDecimal.ZERO); //corpo da instancia a ser retornada	
