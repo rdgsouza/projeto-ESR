9361315b2e479d89ba4e4cbdf6eb4cf4bbe137aa
rodrigo <rdsouza.c@gmail.com>
2021-06-02 15:51:40 -0400

    Projeto souzafood-api

--- souzafood-api/src/main/java/com/souza/souzafood/core/web/WebConfig.java
+++ souzafood-api/src/main/java/com/souza/souzafood/core/web/WebConfig.java
@@ -1,6 +1,10 @@
 package com.souza.souzafood.core.web;
 
+import javax.servlet.Filter;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.ShallowEtagHeaderFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -10,10 +14,15 @@ public class WebConfig implements WebMvcConfigurer {
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 
-		registry.addMapping("/**")
-		    .allowedMethods("*");
+		registry.addMapping("/**").allowedMethods("*");
 //		     .allowedOrigins("*")
 //		.maxAge(30);   
 
 	}
+
+	@Bean
+	public Filter shallowEtagHeaderFilter() {
+		return new ShallowEtagHeaderFilter();
+	}
+
 }
